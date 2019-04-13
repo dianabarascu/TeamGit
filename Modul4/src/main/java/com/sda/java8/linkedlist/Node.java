2a2d08057604314bb65fab55b0a6e5ec7d14098f
Diana <diana_barascu@yahoo.com>
2019-04-13 14:12:08 +0300

    Add node stub

---
+++ Modul4/src/main/java/com/sda/java8/linkedlist/Node.java
@@ -0,0 +1,14 @@
+package com.sda.java8.linkedlist;
+
+public class Node {
+
+    private final int value;
+    private Node nextElement;
+
+    public Node (int value){
+        this.value = value;
+
+    }
+
+
+}
