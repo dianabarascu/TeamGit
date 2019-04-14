dd6aed7581ef8db4d6d2084c5fe2a8b5013410ab
Diana <diana_barascu@yahoo.com>
2019-04-14 10:26:02 +0300

    Implement remove element

--- Modul4/src/main/java/com/sda/java8/linkedlist/Application.java
+++ Modul4/src/main/java/com/sda/java8/linkedlist/Application.java
@@ -7,13 +7,26 @@ public class Application {
         Node node1 = new Node(20);
         Node node2 = new Node(40);
         Node node3 = new Node(50);
+        Node node4 = new Node(30);
 
         LinkedList list = new LinkedList();
+
         list.add(node1);
         list.add(node2);
         list.add(node3);
+        list.add(node4);
 
         System.out.println(list.print());
+
+        list.remove(40);
+        System.out.println(list.print());
+        list.remove(20);
+        System.out.println(list.print());
+        list.remove(50);
+        System.out.println(list.print());
+        list.remove(30);
+        System.out.println(list.print() +  "Ultimul");
+
 
     }
 }
--- Modul4/src/main/java/com/sda/java8/linkedlist/LinkedList.java
+++ Modul4/src/main/java/com/sda/java8/linkedlist/LinkedList.java
@@ -5,40 +5,64 @@ public class LinkedList {
     private Node head;
 
     // Adds new node to the list
-    void add (Node node){
-        // Test if null
-        if (head == null){
+    void add (Node node) {
+
+        if (head == null) {
             head = node;
             return;
         }
         Node parcurge = head;
-        while(parcurge.getNextElement()!= null){
+
+        while (parcurge.getNextElement() != null) {
             parcurge = parcurge.getNextElement();
         }
         parcurge.setNextElement(node);
-        // parcurge = head
-        // while parcurge has a next element
-        //      parcurge becomes parcurge.next
-        // parcurge.next = node
-
 
     }
+
+    void remove (int val) {
+//        // Example remove last element if last contains only 2 values
+//        if(head.getNextElement().getNextElement() == null){
+//            head.setNextElement(null);
+//        }
+
+//        for (Node nodeCurent = head; nodeCurent.getNextElement().getValue()==val){
+//            nodeCurent = nodeCurent.getNextElement();
+//        }
+        // if the list is empty, we stop.
+        if(head == null) {
+            return;
+        }
+        //if the first element is the value to remove, move the head to the next element
+        if (head.getValue() == val){
+            head = head.getNextElement();
+            return;
+        }
+        // Walk the list until we pass through all elements or we find the value
+        Node nodeCurent = head;
+
+        while ((nodeCurent.getNextElement() != null) &&
+                (nodeCurent.getNextElement().getValue() != val)) {
+            nodeCurent = nodeCurent.getNextElement();
+        }
+        // if we didn't find the element, end.
+        if (nodeCurent.getNextElement() == null){
+            return;
+        }
+        //remove the element
+        nodeCurent.setNextElement(nodeCurent.getNextElement().getNextElement());
+    }
+
     // Displays all elements values of the list separated by space
-    String print(){
-//        String elemente = "";
+    String print() {
+
         StringBuilder builder = new StringBuilder();
-        for(Node nodCurent = head ; nodCurent != null;
-        nodCurent = nodCurent.getNextElement()){
+        for (Node nodCurent = head; nodCurent != null; nodCurent = nodCurent.getNextElement()) {
             builder.append(nodCurent.getValue());
             builder.append(" ");
 
         }
         return builder.toString();
-//        Node parcurge = head;
-//        while(parcurge.getNextElement()!= null){
-//            System.out.println(parcurge.getValue());
-//        }
 
-//        return elemente;
     }
 }
