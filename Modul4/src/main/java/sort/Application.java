1666238936885a509e2a6b7369c68961accd5fba
Diana <diana_barascu@yahoo.com>
2019-04-20 13:54:07 +0300

    bubble sort random generated values

--- Modul4/src/main/java/sort/Application.java
+++ Modul4/src/main/java/sort/Application.java
@@ -1,4 +1,58 @@
-package bubblesort;
+package sort;
+
+import java.util.Scanner;
 
 public class Application {
+
+    public static void main(String[] args) {
+
+        int[] myArray = {2, 4, 3};
+        int[] myArray2 = {6, 5, 3, 1, 8, 7, 2, 4};
+        Sort.bubbleSort(myArray);
+        Sort.bubbleSort(myArray2);
+        printArray(myArray);
+        printArray(myArray2);
+
+        int[] myArray3 = generateRandomArray(getCitit());
+        Sort.bubbleSort(myArray3);
+        printArray(myArray3);
+
+    }
+
+    public static void printArray(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] generateRandomArray(int n) {
+
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            int generated = (int) (Math.random() * n);
+            int j = 0;
+            while (j < i) {
+                if (generated == array[j]) {
+                    j = 0;
+                    generated = (int) (Math.random() * n);
+                } else {
+                    j++;
+                }
+            }
+
+            array[i] = generated;
+        }
+        return array;
+    }
+
+    private static int getCitit() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti numarul de elemente: ");
+        int citit = scanner.nextInt();
+        return citit;
+    }
+
 }
--- Modul4/src/main/java/sort/Sort.java
+++ Modul4/src/main/java/sort/Sort.java
@@ -1,10 +1,21 @@
-package bubblesort;
+package sort;
 
-public class BubbleSort {
+public class Sort {
 
-    public static void bubbleSort(int[] array){
+    public static void bubbleSort(int[] array) {
+        boolean executat = false;
+        do {
+            executat = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    executat = true;
+                }
+            }
+        } while (executat);
 
-        
 
     }
 
